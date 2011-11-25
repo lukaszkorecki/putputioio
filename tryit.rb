64e8fa42826420cff3ab9a9257589f36f21fdc69
Łukasz Korecki <lukasz@coffeesounds.com>
2011-11-25 09:45:14 +0000

    how to read folder contents

--- tryit.rb
+++ tryit.rb
@@ -14,5 +14,14 @@ f = dirs['response']['results'].first
 
 dir_id = f['id']
 
-puts putio.get 'files', 'list', { id: f['id']}
+puts  putio.get( 'user', 'acctoken' )#, { parent_id: f['id']})['response']['results'].first
+_fi
+
+
+puts putio.get 'user', 'info'
+puts "curl #{_fi['download_url']} > #{Shellwords::shellescape _fi['name'] }"
+
+
+require 'shellwords'
+Shellwords::shellescape
 
